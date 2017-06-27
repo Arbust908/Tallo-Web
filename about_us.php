ba9451fe39187afe19b7e7d4f2097f0f7097ddf9
Arbust0 <Arbust908@gmail.com>
2017-06-27 14:19:23 -0300

    Inicialización
    -Agregado de todos los archivos iniciales
    -Excluyendo las imagenes con Copyright

---
+++ about_us.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Juan Tallo</title>
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/flexboxgrid.min.css">
+    <link rel="shortcut icon" href="imgs/favicon.ico">
+  </head>
+  <body>
+    <nav class="row center-xs middle-xs end-sm">
+      <div class="col-xs-12 col-sm-2">
+        <a href="index.php" title="Home">
+          <img src="imgs/favicon.png" alt="Juan Tallo">
+        </a>
+      </div>
+      <div class="col-xs-12 col-sm-10">
+        <ul>
+          <li class="selected"><a selected href="index.php">Home</a></li>
+          <li><a href="architectur.php">Architectur</a></li>
+          <li><a href="other.php">Other</a></li>
+          <li><a href="about_us.php">About Us</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <main>
+      <section class="row center-xs">
+        <h1>About Us</h1>
+        <div class="col-xs-12 col-sm-2">
+          <img src="imgs/about_tallo.png" alt="Juan Tallo">
+        </div>
+        <div class="col-xs-12 col-sm-10">
+          <p>
+          ￼Argentinean-Italian Juan Tallo is a multi media artist.<br><br>
+          While completing his degree in Architecture, he started his training in photography in 1985.<br>
+          He opened his Buenos Aires photography studio in 1990, working since then as an Advertising Photographer, Cinematographer and Visual Arts Consultant.<br>
+           As founder and CEO of Tallo & Torres Advertising Photography, he worked with medium and large-format film cameras until his studio introduced the first high-end studio digital camera in South America. Tallo & Torres became one of the most sought after, cutting edge studio for numerous projects, including architecture, industrial, products shots, people, corporate, commercial and aerial photography.<br>
+           His work for an advertising campaign for Image Bank Argentina was recognized as a Finalist of Graphic Photography in the London International Festival.<br>
+           In 1999, he won a Special Award in Architectural Photography and his work was exhibited in the National Museum of Fine Arts, in Buenos Aires.<br>
+           His work was also shown at the Advertising Photographers of America, Photo 12 Awards in Los Angeles.<br>
+           His trained eye and fine sense of composition -honed by his background in architecture- make for a rare combination of visual sensibilities.<br>
+           He moved to Los Angeles, California in 1999, where he also specializes in digital image manipulation, digital printing and direction of photography for documentaries.<br>
+          But back in 1996 he began yet another path of exploration: searching for the delicate balance between form, function, art, light and space which resulted in his “Lighting Sculptures.” These pieces instantly attracted lots of attention.<br>
+          A unique choice of materials, textures and light combined to create unique pieces where tension, balance, peacefulness and simplicity embark in a joyful play of transparencies and shadows.<br>
+          His Lighting Sculptures has been acquired by several prestigious architectural and photographic studios in Los Angeles and San Francisco, as well as by some international companies.<br>
+        </p>
+        </div>
+      </section>
+    </main>
+
+    <footer>
+      <ul>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+      </ul>
+      <p>Copyright &copy; 2017 | Juan Tallo</p>
+    </footer>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="author" content="panchoblanco.com.ar">
+    <meta name="description" content="Juan Tallo is a Profesional Photographer and multimedia creator">
+    <meta name="keywords" content="Photo,Photografy,Juan,Tallo,John,Architecture,Groso" />
+    <title>Juan Tallo</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/flexboxgrid.min.css">
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="shortcut icon" href="imgs/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="row center-xs middle-xs end-sm">
+      <div class="col-xs-12 col-sm-2">
+        <a href="index.php" title="Home">
+          <img src="imgs/favicon.png" alt="Juan Tallo">
+        </a>
+      </div>
+      <div class="col-xs-12 col-sm-10">
+        <ul>
+          <li><a class="selected" href="index.php">Home</a></li>
+          <li><a href="architectur.php">Architectur</a></li>
+          <li><a href="other.php">Other</a></li>
+          <li><a href="about_us.php">About Us</a></li>
+        </ul>
+      </div>
+    </nav>
+    <main>
+      <section id="Arch" class="row center-xs middle-xs">
+        <h1>Architectur</h1>
+        <div class="col-xs-12">
+          <h1><a href="#">Architectur</a></h1>
+          <img src="imgs/JT_06540a.jpg" alt="imagen" title="Architectur">
+        </div>
+      </section>
+      <section id="Other" class="row center-xs middle-xs">
+        <h1>OTHER</h1>
+        <div class="col-xs-12">
+          <h1><a href="#">Travel</a></h1>
+          <img src="imgs/JT_06540a.jpg" alt="imagen" title="Travel">
+        </div>
+        <div class="col-xs-12">
+          <h1><a href="#">Food</a></h1>
+          <img src="imgs/JT_06540a.jpg" alt="imagen" title="Food">
+        </div>
+        <div class="col-xs-12">
+          <h1><a href="#">People</a></h1>
+          <img src="imgs/JT_06540a.jpg" alt="imagen" title="People">
+        </div>
+        <div class="col-xs-12">
+          <h1><a href="#">Product</a></h1>
+          <img src="imgs/JT_06540a.jpg" alt="imagen" title="Product">
+        </div>
+        <div class="col-xs-12">
+          <h1><a href="#">Fine art</a></h1>
+          <img src="imgs/JT_06540a.jpg" alt="imagen" title="Fine art">
+        </div>
+        <div class="col-xs-12">
+          <h1><a href="#">Motion</a></h1>
+          <img src="imgs/JT_06540a.jpg" alt="imagen" title="Motion">
+        </div>
+      </section>
+    </main>
+    <footer class="row center-xs bottom-xs">
+      <ul>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+      </ul>
+      <p>Copyright &copy; 2017 | Juan Tallo</p>
+    </footer>
+  </body>
+</html>
